124100bc66977b06bd425217cc98dfe5a678924a
Maciej Kowalski <maciej33kowalski@gmail.com>
2017-01-15 22:03:32 +0100

    fix AccountViewSet#get_permissions (working registration!!)

--- authentication/views.py
+++ authentication/views.py
@@ -13,12 +13,12 @@ class AccountViewSet(viewsets.ModelViewSet):
 
     def get_permissions(self):
         if self.request.method in permissions.SAFE_METHODS:
-            return (permissions.AllowAny())
+            return (permissions.AllowAny(),)
 
         if self.request.method == 'POST':
-            return (permissions.AllowAny())
+            return (permissions.AllowAny(),)
 
-        return permissions.IsAuthenticated(), IsAccountOwner()
+        return (permissions.IsAuthenticated(), IsAccountOwner(),)
 
     def create(self, request):
         serializer = self.serializer_class(data=request.data)
