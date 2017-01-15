8a219eb84f8c1edd2ad9cd78c23cdfb945b5d48f
Maciej Kowalski <maciej33kowalski@gmail.com>
2017-01-15 17:55:56 +0100

    update urls.py, use AccountViewSet instead of IndexView (default), use SimpleRouter

--- thinkster_django_angular_boilerplate/urls.py
+++ thinkster_django_angular_boilerplate/urls.py
@@ -1,9 +1,16 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import include, patterns, url
+from rest_framework_nested import routers
 
-from thinkster_django_angular_boilerplate.views import IndexView
+from authentication.views import AccountViewSet
+
+
+router = routers.SimpleRouter()
+router.register(r'accounts', AccountViewSet)
 
 urlpatterns = patterns(
     '',
 
-    url('^.*$', IndexView.as_view(), name='index'),
+    url(r'^api/v1/', include(router.urls)),
+
+    url('^.*$', AccountViewSet.as_view(), name='index'),
 )
