c0a129a30ee7739221eea8a74bc0f47c1750662a
Maciej Kowalski <maciej33kowalski@gmail.com>
2017-01-16 21:09:34 +0100

    add activate fn to register.controller.js

--- static/javascripts/authentication/controllers/register.controller.js
+++ static/javascripts/authentication/controllers/register.controller.js
@@ -19,6 +19,18 @@
 
         vm.register = register;
 
+        activate();
+
+        /**
+         * @name activate
+         * @desc Actions to be performed when this controller is instantiated
+         * @memberOf thinkster.authentication.controllers.RegisterController
+         */
+        function activate() {
+            if (Authentication.isAuthenticated()) {
+                $location.url('/');
+            }
+        };
         /**
          * @name register
          * @desc Register a new user
