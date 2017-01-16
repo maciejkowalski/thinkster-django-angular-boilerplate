e387e010f1fcdb7dc4e1d6d037760713de22a833
Maciej Kowalski <maciej33kowalski@gmail.com>
2017-01-16 18:56:23 +0100

    declare /login URL in urls.py

--- thinkster_django_angular_boilerplate/urls.py
+++ thinkster_django_angular_boilerplate/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import include, patterns, url
 from rest_framework_nested import routers
 
-from authentication.views import AccountViewSet
+from authentication.views import AccountViewSet, LoginView
 from thinkster_django_angular_boilerplate.views import IndexView
 
 
@@ -10,6 +10,8 @@ router.register(r'accounts', AccountViewSet)
 
 urlpatterns = patterns(
     '',
+
+    url(r'^api/v1/auth/login/$', LoginView.as_view(), name='login'),
 
     url(r'^api/v1/', include(router.urls)),
 
