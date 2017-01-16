ff68b0ef9e5b09cf12ae46bdb889c9c1ed0fea5f
Maciej Kowalski <maciej33kowalski@gmail.com>
2017-01-16 21:52:01 +0100

    update urls.py so it includes LogoutView

--- thinkster_django_angular_boilerplate/urls.py
+++ thinkster_django_angular_boilerplate/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import include, patterns, url
 from rest_framework_nested import routers
 
-from authentication.views import AccountViewSet, LoginView
+from authentication.views import AccountViewSet, LoginView, LogoutView
 from thinkster_django_angular_boilerplate.views import IndexView
 
 
@@ -12,6 +12,8 @@ urlpatterns = patterns(
     '',
 
     url(r'^api/v1/auth/login/$', LoginView.as_view(), name='login'),
+
+    url(r'^api/v1/auth/logout/$', LogoutView.as_view(), name='logout'),
 
     url(r'^api/v1/', include(router.urls)),
 
