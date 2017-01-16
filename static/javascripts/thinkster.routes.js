90d85058f134c9062c1f0af418d112d52a7c1a5b
Maciej Kowalski <maciej33kowalski@gmail.com>
2017-01-16 21:11:53 +0100

    expand Angular routes

--- static/javascripts/thinkster.routes.js
+++ static/javascripts/thinkster.routes.js
@@ -11,12 +11,15 @@
      * @name config
      * @desc Define valid application routes
      */
-
     function config($routeProvider) {
         $routeProvider.when('/register', {
             controller: "RegisterController",
             controllerAs: "vm",
             templateUrl: "static/templates/authentication/register.html"
+        }).when('/login', {
+            controller: "LoginController",
+            controllerAs: "vm",
+            templateUrl: "static/templates/authentication/login.html"
         }).otherwise('/');
     };
 })();
