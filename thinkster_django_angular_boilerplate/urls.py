313281ee84aea5f3958e4935a90f3dfc8b51da1a
Maciej Kowalski <maciej33kowalski@gmail.com>
2017-01-19 18:19:34 +0100

    update urls.py so it includes Posts views

--- thinkster_django_angular_boilerplate/urls.py
+++ thinkster_django_angular_boilerplate/urls.py
@@ -2,11 +2,18 @@ from django.conf.urls import include, patterns, url
 from rest_framework_nested import routers
 
 from authentication.views import AccountViewSet, LoginView, LogoutView
+from posts.views import PostView, AccountPostsViewSet
 from thinkster_django_angular_boilerplate.views import IndexView
 
 
 router = routers.SimpleRouter()
 router.register(r'accounts', AccountViewSet)
+router.register(r'posts', PostView)
+
+accounts_router = routers.NestedSimpleRouter(
+    router, r'accounts', lookup='account'
+)
+accounts_router.register(r'posts', AccountPostsViewSet)
 
 urlpatterns = patterns(
     '',
@@ -16,6 +23,7 @@ urlpatterns = patterns(
     url(r'^api/v1/auth/logout/$', LogoutView.as_view(), name='logout'),
 
     url(r'^api/v1/', include(router.urls)),
+    url(r'^api/v1', include(accounts_router.urls)),
 
     url('^.*$', IndexView.as_view(), name='index'),
 )
