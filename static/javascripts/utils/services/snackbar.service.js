8415ed96746e10df9c796cd7271f78141ce24b99
Maciej Kowalski <maciej33kowalski@gmail.com>
2017-01-20 19:20:23 +0100

    add snackbar.service.js

---
+++ static/javascripts/utils/services/snackbar.service.js
@@ -0,0 +1,28 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('thinkster.utils.services')
+        .factory("Snackbar", Snackbar);
+
+    function Snackbar() {
+        var Snackbar = {};
+
+        return Snackbar;
+
+        function _snackbar(content, options) {
+            options = _.extend({timeout: 3000}, options);
+            options.content = content;
+
+            $.snackbar(options);
+        }
+
+        function error(content, options) {
+            _snackbar('Error ' + content, options);
+        }
+
+        function show(content, options) {
+            _snackbar(content, options);
+        }
+    }
+})($, _);
